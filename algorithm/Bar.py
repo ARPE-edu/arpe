1475359dcbf73db5b48348dedec68fc118faf1e6
ARPE-edu <84019159+ARPE-edu@users.noreply.github.com>
2021-05-20 16:42:03 +0200

    Delete algorithm directory (#19)

--- algorithm/Bar.py
+++
@@ -1,8 +0,0 @@
-import time
-from progress.bar import IncrementalBar
-mylist = [1,2,3,4,5,6,7,8]
-bar = IncrementalBar(max = len(mylist))
-for item in mylist:
-    bar.next()
-    time.sleep(1)
-bar.finish()
