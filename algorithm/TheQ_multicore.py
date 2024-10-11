9b0d7248132b90e592cf1b84efd36fd2bfa7bee3
ARPE-edu <84019159+ARPE-edu@users.noreply.github.com>
2024-10-11 19:20:44 +0200

    Update TheQ_multicore.py
    Error Exception

--- algorithm/TheQ_multicore.py
+++ algorithm/TheQ_multicore.py
@@ -6,45 +6,42 @@ import concurrent.futures
 import os
 
 
-def TheQFuntion(filelocation):
-    # # Return structure:
-    # [ListofFiles, WCCFXList, PlotDataList, Q0, DataToSave, Corrupt] = Q(basepath)
-    ListofFiles, WCCFXList, PlotDataList, Q0, Corrupt =  ([] for i in range(5))
-    DataToSave=pd.DataFrame() 
-    
-    print('This is the filelocation' + filelocation)
+# Helper function to wrap Q and handle exceptions
+def safe_Q(file_path):
+    try:
+        return Q(file_path)
+    except Exception as e:
+        # Return some default error structure to signify failure
+        return ([], [], [], [], pd.DataFrame(), [str(e)])
 
+
+def TheQFuntion(filelocation):
+    ListofFiles, WCCFXList, PlotDataList, Q0, Corrupt = ([] for i in range(5))
+    DataToSave = pd.DataFrame()
+    
     o = 0
-    ListofPaths=[]
+    ListofPaths = []
     try:
         for entry in os.listdir(filelocation):
             if entry.endswith(".s2p") or entry.endswith(".S2P"):
                 o += 1
-                full_path = os.path.join(filelocation,entry)
+                full_path = os.path.join(filelocation, entry)
                 if os.path.isfile(full_path):
                     ListofPaths.append(full_path)
-                    print(str(o)+'.'+entry)
     except NotADirectoryError:
-        if os.path.isfile(os.path.join(filelocation)):
+        if os.path.isfile(filelocation):
             ListofPaths.append(filelocation.split('/')[-1])
 
-    print(ListofPaths)
-   
-    
-    # Calling Q with multiprocess
+    # Calling Q with multiprocess using safe_Q wrapper
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        # Submit callables to the executor
-        #for f in files:
-        future = executor.map(Q,ListofPaths)
-        # Retrieve the results
+        future = executor.map(safe_Q, ListofPaths)
+
         for f in future:
             loff, wcc, plda, qq0, dts, cor = f
             [lst.extend(element)
-                for lst,element 
-                in zip([ListofFiles, WCCFXList, PlotDataList, Q0, Corrupt],
+             for lst, element
+             in zip([ListofFiles, WCCFXList, PlotDataList, Q0, Corrupt],
                     [loff, wcc, plda, qq0, cor])]
             DataToSave = pd.concat([DataToSave, dts], ignore_index=True)
-    
-    
 
-    return ListofFiles, WCCFXList, PlotDataList, Q0, DataToSave , Corrupt
+    return ListofFiles, WCCFXList, PlotDataList, Q0, DataToSave, Corrupt
