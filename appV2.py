8f68933825cf84536f4063258163142d11b9d9dd
MartinHerold <martinherold1992@gmail.com>
2026-01-18 10:24:41 +0100

    app.run for global environment

--- appV2.py
+++ appV2.py
@@ -787,6 +787,6 @@ def display_confirm(value):
 """ Run it """
 if __name__ == '__main__':
     ####### global environment
-    # app.run(port=8050,debug=False,host='0.0.0.0')
+    app.run(port=8050,debug=False,host='0.0.0.0')
     ####### local environment
     # app.run(port=8050, debug=False)
